695892e3514806f941ca3fd3141c9afc91decac9
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2017-09-09 12:32:25 -0500

    Rooms

---
+++ Super nombre iraqui/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+	public void Play()
+    {
+        Application.LoadLevel("SelectLvl");
+    }
+    public void Store()
+    {
+        Application.LoadLevel("Store");
+    }
+    public void Settings()
+    {
+        Application.LoadLevel("Settings");
+    }
+    public void Volver()
+    {
+        Application.LoadLevel("Main Menu");
+    }
+    public void LVL1()
+    {
+        Application.LoadLevel("Lvl1");
+    }
+}
